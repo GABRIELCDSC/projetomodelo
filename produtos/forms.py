dfad732ec7600938cef011ae31a3861123d39204
Camargo <gabrielcarvalhocamargo007@gmail.com>
2024-11-25 08:24:36 -0300

    first commit

---
+++ produtos/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from produtos.models import Categoria, Produto
+
+class CategoriaForm(forms.ModelForm):
+    class Meta:
+        model = Categoria
+        fields = ['nome']
+        labels = {
+            'nome': 'Nome da categoria',
+        }
+
+class ProdutoForm(forms.ModelForm):
+    class Meta:
+        model = Produto
+        fields = ['nome', 'preco', 'categoria', 'imagem']
+        labels = {
+            'nome': 'Nome do produto',
+            'preco': 'Preço (R$)',
+            'categoria': 'Categoria',
+            'imagem': 'Imagem', 
+        }
